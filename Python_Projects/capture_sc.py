c4d54c1dc94a21301ff9c8e76c5a0d7fe2f966a1
Rubiks05 <antoniidaressurreicao@gmail.com>
2025-02-04 00:32:44 -0300

    Update

---
+++ Python_Projects/capture_sc.py
@@ -0,0 +1,52 @@
+import subprocess
+import random
+from separator import separator
+
+# Adicionando meus diretórios primários:
+# "fake" é usado como diretório que não existe.
+# "nothing" será usado como configuração para não ter nenhum caminho.
+primary_directories = {}
+list = ["Desktop", "Documents", "Pictures", "Music", "Downloads", "Public", "Templates", "Videos", "snap", "fake", "nothing"]
+for i in list:
+    primary_directories[i] = []
+
+# Adicionando diferentes caminhos:
+primary_directories["Documents"].extend(["circuitos", "Rubiks05.github.io", "Strong-Password-Generator", "Computer-Science-Freshmen-at-UFPR", "Scientific-Calculator", "Sorting-and-Searching-Algorithms", "The-Boys"])
+primary_directories["Pictures"].extend(["Images", "Screenshots"])
+primary_directories["snap"].extend(["firefox", "snapd-desktop-integration", "spotify"])
+primary_directories["Downloads"].extend(["coord.o", "entrada2.txt", "forrest-sense-of-self-1.pdf", "Generator2.py", "index.html", "outfile.tga", "testes", "dark_room.png", "entrada.txt", "forrest-sense-of-self.pdf", "Generator.py", "main.py", "strace_r.txt"])
+
+resultado = []
+comandos = []
+for i in range(50):
+    random_primary_directory = random.choice(list)
+
+    there_isnt_more = False
+    if len(primary_directories[random_primary_directory]) != 0:
+        random_path = random.choice(primary_directories[random_primary_directory])
+    else:
+        there_isnt_more = True
+    
+
+    if not there_isnt_more:
+        path = "/"+random_primary_directory+"/"+random_path
+    else:
+        #então nós queremos realmente que não seja nada.
+        if random_primary_directory == "nothing":
+            path = ""
+        else:
+            path = "/"+random_primary_directory
+
+    comando = "strace mv "+path
+    comandos.append(comando)
+
+    processo = subprocess.run(comando, shell=True, capture_output=True, text=True)
+
+    resultado_preliminar = separator(processo.stderr)
+    resultado.extend(resultado_preliminar)
+
+print(resultado)
+print()
+print("-------------------------------------")
+print()
+print(comandos)
--- Python_Projects/separator.py
+++ Python_Projects/separator.py
@@ -1,23 +1,13 @@
+import re
 # Recebendo o texto inteiro ao dar Strace.
-entradas = []
-while (True):
-    string = str(input())
-    if string == "":
-        break
-    entradas.append(string)
+def separator(entradas):
+    nomes = re.findall(r'(\w+)\s*\(', entradas)
 
-resultado = []
-for line in entradas:
-    #conferindo se os elementos de entradas realmente contém a estrutura de um System Call ('chamada()')
-    eh_system_call = False
-    if '(' in line and ')' in line:
-        eh_system_call = True
+    #tirando nomes desnecessários:
+    for word in nomes:
+        if word.isupper():
+            nomes.remove(word)
 
-    if eh_system_call:    
-        interesse = line.split("(", 1)[0]
-        resultado.append(interesse)
-        
-print(", ".join(resultado))
-print()
-print(" ".join(resultado))
+
+    return nomes
 
