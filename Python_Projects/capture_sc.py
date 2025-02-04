4e3dcb689a38804911f4beeda3deb2f7880facac
Rubiks05 <antoniidaressurreicao@gmail.com>
2025-02-04 01:12:21 -0300

    Update

--- Python_Projects/capture_sc.py
+++ Python_Projects/capture_sc.py
@@ -16,9 +16,12 @@ primary_directories["Pictures"].extend(["Images", "Screenshots"])
 primary_directories["snap"].extend(["firefox", "snapd-desktop-integration", "spotify"])
 primary_directories["Downloads"].extend(["coord.o", "entrada2.txt", "forrest-sense-of-self-1.pdf", "Generator2.py", "index.html", "outfile.tga", "testes", "dark_room.png", "entrada.txt", "forrest-sense-of-self.pdf", "Generator.py", "main.py", "strace_r.txt"])
 
+#Opções que podem ser escolhidas com o comando:
+ls_options = ["", "-a", "-l", "-R", "-i", "-lh", "-l", "-1", "-s", "-t", "/", "..", "-la", "-S", "*.txt", "*", "-R -l"]
+
 resultado = []
 comandos = []
-for i in range(1000):
+for i in range(10000):
     random_primary_directory = random.choice(list)
 
     there_isnt_more = False
@@ -37,7 +40,9 @@ for i in range(1000):
         else:
             path = "/"+random_primary_directory
 
-    comando = "strace mv "+path
+    option_ls = random.choice(ls_options)
+
+    comando = "strace ls "+option_ls+" "+path
     comandos.append(comando)
 
     processo = subprocess.run(comando, shell=True, capture_output=True, text=True)
@@ -45,10 +50,10 @@ for i in range(1000):
     resultado_preliminar = separator(processo.stderr)
     resultado.extend(resultado_preliminar)
 
-with open('/home/toninho/Documents/Systems-Calls-Project/Dates/mv/date_mv.txt', 'w') as file1:
+with open('/home/toninho/Documents/Systems-Calls-Project/Dates/ls/date_ls.txt', 'w') as file1:
     file1.write(" ".join(resultado))
 
-with open('/home/toninho/Documents/Systems-Calls-Project/Dates/mv/commands.txt', 'w') as file2:
+with open('/home/toninho/Documents/Systems-Calls-Project/Dates/ls/commands.txt', 'w') as file2:
     for i in range(len(comandos)):
         file2.write("{}. {}\n".format(i+1, comandos[i]))
 
