5e5e9ee756c642dec92a2bc457e0b83b52790b18
Rubiks05 <antoniidaressurreicao@gmail.com>
2025-02-22 00:37:12 -0300

    Update

--- Python_Projects/create_emails.py
+++ Python_Projects/create_emails.py
@@ -1,13 +1,29 @@
 tamanho_arquivo = 50000
 
-texto_base = "This is a test writed only in ASCII!"
+texto_base = "This is a test that I will encrypt in PGP"
 
 repeticoes = tamanho_arquivo // len(texto_base)
 resto = tamanho_arquivo % len(texto_base)
 
 conteudo = (texto_base * repeticoes) + texto_base[:resto]
 
-with open("/home/toninho/Documents/Systems-Calls-Project/Dates/sendmail/email3.txt", "w") as arquivo:
+with open("/home/toninho/Documents/Systems-Calls-Project/Dates/sendmail/email8.txt", "w") as arquivo:
     arquivo.write(conteudo)
+
+lista = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 print("Success!")
